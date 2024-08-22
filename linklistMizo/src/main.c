64f4d917d330396db40c83dd72a22afbd21ac42d
taitesen <iamhmunhlei@gmail.com>
2024-08-22 22:21:41 +0530

    optimized code

--- linklistMizo/src/main.c
+++ linklistMizo/src/main.c
@@ -5,7 +5,7 @@
 
 int main(){
     int choice = 0;
-    int position;
+    int position = 0;
     int counter = 0;
     char message[100];
     do {
@@ -22,39 +22,23 @@ int main(){
                 printf("Update tur a awm rih lo\n");
             else if (choice == 3 && (position > counter))
                 printf("%d na a awm lo. Update theih loh.\n", position);
+            else if(choice < 1 || choice > 4)
+                printf("%d HI A ENTER THEIH LOH\n", choice );
             else
-                printf("ERRORS A AWM RIH LOVE\n");
+                printf("1 to 4 A THLAN THEIH\n");
         }
+
         printf("\n");
         printf("1. Add list.\n");
         printf("2. Delete list.\n");
         printf("3. Update list.\n");
         printf("4. Exit.\n");
         printf("\n");
-        printf("1 to 4 enter rawh: ");
+        printf("[ 1 to 4 ] duh ber enter rawh: ");
         scanf("%d", &choice);
         ClearBuffer();
-
-        position = 0;
-        counter = 0;
         strcpy(message, "");
 
-        while( choice < 1 || choice > 4 ){
-            system("clear");
-            printf(" -- TODO LIST MIZO VERSION --\n");
-            Show();
-            printf("\n");
-            printf("%d HI A THLAN THEIH LOH.\n", choice);
-            printf("\n");
-            printf("1. Add list.\n");
-            printf("2. Delete list.\n");
-            printf("3. Update list.\n");
-            printf("4. Exit.\n");
-            printf("\n");
-            printf("1 atanga 4 chiah a thlan theih: ");
-            scanf("%d", &choice);
-            ClearBuffer();
-        }
         switch (choice) {
             case 1:{
                 char buffer[120];
@@ -84,6 +68,7 @@ int main(){
                     break;
                 }
                 Delete(position);
+                position = 0;
                 break;
             }
             case 3:{
@@ -122,6 +107,7 @@ int main(){
                         strncpy(message, buffer, sizeof(message)/sizeof(message[0]) - 1 );
                 }
                 UpdateAtPos(message, position);
+                position = 0;
                 break;
             }
             case 4:{
